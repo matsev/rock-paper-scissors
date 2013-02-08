2cb063a40281a0fe7cf3dd2953a5835652ff3a67
Anders Eriksson <anders.eriksson@gmail.com>
2013-02-08 12:03:51 +0100

    game aggregate

--- src/main/java/com/jayway/rps/GameAggregate.java
+++ src/main/java/com/jayway/rps/GameAggregate.java
@@ -1,7 +1,29 @@
 package com.jayway.rps;
 
+import com.jayway.rps.command.CommandHandler;
+import com.jayway.rps.command.CreateGame;
+import com.jayway.rps.event.Event;
+import com.jayway.rps.event.EventHandler;
+import com.jayway.rps.event.GameCreated;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Anders Eriksson.
  */
 public class GameAggregate  {
+
+    private UUID gameId;
+
+    @EventHandler
+    public void handle(GameCreated gameCreated) {
+        this.gameId = gameCreated.getEntityId();
+    }
+
+    @CommandHandler
+    public List<? extends Event> handle(CreateGame createGame) {
+        return Arrays.asList(new GameCreated(createGame.entityId()));
+    }
 }
--- src/main/java/com/jayway/rps/event/GameCreated.java
+++ src/main/java/com/jayway/rps/event/GameCreated.java
@@ -1,4 +1,18 @@
 package com.jayway.rps.event;
 
+import java.util.UUID;
+
 public class GameCreated implements Event {
+
+
+    private final UUID entityId;
+
+    public GameCreated(UUID entityId) {
+        this.entityId = entityId;
+    }
+
+
+    public UUID getEntityId() {
+        return entityId;
+    }
 }
